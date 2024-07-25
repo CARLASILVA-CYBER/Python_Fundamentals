0028a1809a330603b2044f3b979208281b9752ea
Carla Silva <61299273+CARLASILVA-CYBER@users.noreply.github.com>
2024-07-25 18:14:02 +0100

    Update operações com vetores e matrizes.py

--- operações com vetores e matrizes.py
+++ operações com vetores e matrizes.py
@@ -2,16 +2,16 @@ import numpy as np
 if __name__=="__main__":
   vetor=np.array([2,5.2,7.8,9])
   MultPortEscalar=7.8*vetor
-  print(MultPorEscalar)
-  print(vetorshape)
+  print(MultPortEscalar)
+  print(vetor.shape)
   matriz=np.array([
   [2,0,4,8],
   [4,5,7,9],
   [2,3,5,0]])
-  print(matriz=np.array.shape)
+  print(matriz.shape)
   print(matriz)
   a=matriz
-  b=np.array([1,0,5,7], [2,4,0,1], [3,0,0,4]])
+  b=np.array([[1,0,5,7], [2,4,0,1], [3,0,0,4]])
   print(a*b)
   print(a+b)
   print(a-b)
@@ -19,4 +19,3 @@ if __name__=="__main__":
   print (np.dot(a,c))
   d=np.array([[1,2], [3,4]])
   print (round(np.linalg.det(d),2))
-
