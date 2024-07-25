653499044621ea4a772066576bd3079adc4d953c
Carla Silva <61299273+CARLASILVA-CYBER@users.noreply.github.com>
2024-07-25 18:19:01 +0100

    Broadcasting_matrizes.py

--- broadcasting_matrizes.py
+++ broadcasting_matrizes.py
@@ -2,7 +2,6 @@ import numpy as np
 
 if __name__ == '__main__':
   
-	a=np.arange(1,25, 2).reshape(4,3)
-  b=np.arange(1,11, 3).reshape(4,1)
-  print("a+b\n", a+b)
-
+    a=np.arange(1,25, 2).reshape(4,3)
+    b=np.arange(1,11, 3).reshape(4,1)
+    print("a+b\n", a+b)
